bda8d459f853d0c7557bb52237453755f98adf27
Jody Currie <jody.currie@gmail.com>
2022-02-17 20:14:53 +0000

    changes made from the theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    
+    //parse current year from current date
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //check to ensure principal amount is positive
+        if (principal <= 0)
+            { 
+            alert("Enter a positive number");
+            document.getElementById("principal").focus()
+            return false();
+            }
+    
+    //display results 
+        document.getElementById("result").innerHTML= "If you deposit <b>" +principal+
+          "</b>,\<br\>at an interest rate of <b>"+rate+"% </b> \<br\>You will receive " +
+          "an amount of <b>"+interest+"</b>,\<br\>in the year <b>"+year+"</b>\<br\>"
+
     
 }
         
+function updateRate()
+{
+    //display rate value when slider is adjusted. 
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
